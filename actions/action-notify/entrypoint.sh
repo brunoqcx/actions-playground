58c12efd0111ca16fe6f9d1f4093c556636b3599
Brunoqcx <brunotavm@gmail.com>
2019-12-12 21:28:25 -0300

    agora vai3

--- actions/action-notify/entrypoint.sh
+++ actions/action-notify/entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/sh -l
 
 echo "segura o meu zindex $MY_NAME"
-curl -X POST -H 'Content-type: application/json' --data '{"text":"$MY_NAME criou um novo PR"}' $SLACK_WEBHOOK
+curl -X POST -H 'Content-type: application/json' --data "{'text':'$MY_NAME criou um novo PR'}" $SLACK_WEBHOOK
